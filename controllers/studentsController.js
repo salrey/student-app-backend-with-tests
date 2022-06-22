4df6d2a52e14c2989981d3f1b60ec875b9efc0a5
salrey <tekdehn@gmail.com>
2022-06-22 15:05:28 -0400

    Set up route to get single student

--- controllers/studentsController.js
+++ controllers/studentsController.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const req = require('express/lib/request');
 
 //Router is capitalized since it's a constructor object
 const controller = express.Router();
@@ -7,6 +8,10 @@ const studentData = require('../studentData.json');
 // for every request there's a response 
 controller.get('/', (request, response) => {
     response.json(studentData)
+})
+
+controller.get('/:id', (request, response) => {
+    response.json(studentData.students.find((student) => student?.id === request.params.id))
 })
 
 //send the controller to app.js 
