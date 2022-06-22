43b1a373ba45d95e1ddbdc159cab2688cf08f791
salrey <tekdehn@gmail.com>
2022-06-22 15:04:35 -0400

    setup database file

--- controllers/studentsController.js
+++ controllers/studentsController.js
@@ -3,9 +3,10 @@ const express = require('express');
 //Router is capitalized since it's a constructor object
 const controller = express.Router();
 
+const studentData = require('../studentData.json');
 // for every request there's a response 
 controller.get('/', (request, response) => {
-    response.send('Hello from the students controller!')
+    response.json(studentData)
 })
 
 //send the controller to app.js 
